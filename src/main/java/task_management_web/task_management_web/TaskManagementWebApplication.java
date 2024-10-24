b674079170cea1f54ed20a0a3158006acb42cd36
Dong <nhutdong9a3@gmail.com>
2024-10-24 22:44:23 +0700

    First commit to git to initialize Spring boot project

---
+++ src/main/java/task_management_web/task_management_web/TaskManagementWebApplication.java
@@ -0,0 +1,13 @@
+package task_management_web.task_management_web;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TaskManagementWebApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TaskManagementWebApplication.class, args);
+	}
+
+}
---
+++ src/test/java/task_management_web/task_management_web/TaskManagementWebApplicationTests.java
@@ -0,0 +1,13 @@
+package task_management_web.task_management_web;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TaskManagementWebApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
