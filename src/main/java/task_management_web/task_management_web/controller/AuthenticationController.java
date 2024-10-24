d65bd1a02df36f66a64e18b8bc56264c0ac621ea
Dong <nhutdong9a3@gmail.com>
2024-10-24 23:12:48 +0700

    Add neww AuthenticationController and AuthenticationService

---
+++ src/main/java/task_management_web/task_management_web/controller/AuthenticationController.java
@@ -0,0 +1,9 @@
+package task_management_web.task_management_web.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/auth")
+public class AuthenticationController {
+}
---
+++ src/main/java/task_management_web/task_management_web/service/AuthenticationService.java
@@ -0,0 +1,7 @@
+package task_management_web.task_management_web.service;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class AuthenticationService {
+}
