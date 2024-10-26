a6e0fe45ebb7f3a5a658eb2cef15269711ef73c9
Dong <nhutdong9a3@gmail.com>
2024-10-26 09:48:16 +0700

    Update AuthenticationController and AuthenticationService for better clean code and method

--- src/main/java/task_management_web/task_management_web/controller/AuthenticationController.java
+++ src/main/java/task_management_web/task_management_web/controller/AuthenticationController.java
@@ -1,8 +1,10 @@
 package task_management_web.task_management_web.controller;
 
+import jakarta.validation.Valid;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import task_management_web.task_management_web.DTO.LoginDTO;
 import task_management_web.task_management_web.DTO.UserDTO;
 import task_management_web.task_management_web.service.AuthenticationService;
 
@@ -12,25 +14,25 @@ public class AuthenticationController {
 
     private final AuthenticationService authenticationService;
 
+    // Constructor injection
     public AuthenticationController(AuthenticationService authenticationService) {
         this.authenticationService = authenticationService;
     }
 
     //Post method for Register
     @PostMapping("/register")
-    public ResponseEntity<String> registerUser(@RequestBody UserDTO userDTO) {
+    public ResponseEntity<String> registerUser( @Valid @RequestBody UserDTO userDTO) {
+
         if (authenticationService.register(userDTO)) {
             return new ResponseEntity<>("Register successfully" , HttpStatus.OK);
         }
-        return new ResponseEntity<>("Register failed" , HttpStatus.BAD_REQUEST);
+        return new ResponseEntity<>("Register failed", HttpStatus.BAD_REQUEST);
     }
 
     //Post method for Login
     @PostMapping("/login")
-        public ResponseEntity<String> loginUser(@RequestBody UserDTO userDTO) {
-        if (authenticationService.login(userDTO)) {
-            return new ResponseEntity<>("Login successfully" , HttpStatus.OK);
-        }
-        return new ResponseEntity<>("Login failed" , HttpStatus.BAD_REQUEST);
+        public ResponseEntity<String> loginUser (@Valid @RequestBody LoginDTO loginDTO) {
+        authenticationService.login(loginDTO);
+        return new ResponseEntity<>("Login successfully", HttpStatus.ACCEPTED);
         }
 }
--- src/main/java/task_management_web/task_management_web/service/AuthenticationService.java
+++ src/main/java/task_management_web/task_management_web/service/AuthenticationService.java
@@ -2,20 +2,33 @@ package task_management_web.task_management_web.service;
 
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
+import task_management_web.task_management_web.DTO.LoginDTO;
 import task_management_web.task_management_web.DTO.UserDTO;
 import task_management_web.task_management_web.entity.UserEntity;
+import task_management_web.task_management_web.exception.AccountAlreadyExistException;
+import task_management_web.task_management_web.exception.AccountNotApprovedException;
+import task_management_web.task_management_web.exception.AuthenticationFailedException;
+import task_management_web.task_management_web.exception.UserNotFoundException;
 import task_management_web.task_management_web.mapper.UserMapper;
 import task_management_web.task_management_web.repository.UserRepository;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import java.time.LocalDate;
+
 
 @Service
 public class AuthenticationService {
-
-
-    //Using constructor-based injection
     private final UserRepository userRepository;
+
     private final PasswordEncoder passwordEncoder;
+
     private final UserMapper userMapper;
 
+    //Using logger for exception error
+    private static final Logger logger = LoggerFactory.getLogger(AuthenticationService.class);
+
+
+    //Using constructor injection
     public AuthenticationService(UserRepository userRepository, PasswordEncoder passwordEncoder, UserMapper userMapper) {
         this.userRepository = userRepository;
         this.passwordEncoder = passwordEncoder;
@@ -31,28 +44,56 @@ public class AuthenticationService {
     // Function for register
     public boolean register (UserDTO userDTO) {
 
-        if(userRepository.existsByEmail(userDTO.getEmail())) {
+        try {
+            if(existsByEmail(userDTO.getEmail())) {
+                throw new AccountAlreadyExistException("Email already exists");
+            }
+
+            if (userDTO.getPassword() == null || userDTO.getPassword().isEmpty()) {
+                throw new IllegalArgumentException("Password cannot be null or empty");
+            }
+
+            // Convert DTO to Entity when working with Repository (User)
+            UserEntity newuserEntity = userMapper.toEntity(userDTO);
+
+            //Set user status to Pending when they register
+            if (newuserEntity.getStatus() == null) {
+                newuserEntity.setStatus(UserEntity.Status.PENDING);
+            }
+
+            //Encoded password before save to database
+            newuserEntity.setPassword(passwordEncoder.encode(userDTO.getPassword()));
+            newuserEntity.setCreated_at(LocalDate.now());
+
+            userRepository.save(newuserEntity);
+            return true;
+
+        }
+        //Print out the error if exists
+        catch (Exception e) {
+            logger.error("An error occurred: ", e);
             return false;
         }
-        // Convert DTO to Entity when working with Repository (User)
-        UserEntity newuserEntity = userMapper.toEntity(userDTO);
-        newuserEntity.setPassword(passwordEncoder.encode(newuserEntity.getPassword()));
-        userRepository.save(newuserEntity);
-        return true;
+
     }
 
     // Function Login Logic
-    public boolean login (UserDTO userDTO) {
+    public void login (LoginDTO loginDTO) {
 
-        UserEntity userEntity = userRepository.findByEmail(userDTO.getEmail())
-                .orElse(null);
+        // Find User in Repository or in database if not found throw exception
+        UserEntity userEntity = userRepository.findByEmail(loginDTO.getEmail())
+                .orElseThrow(() -> new UserNotFoundException("User not found"));
 
-        // Return true if found user, false if not found
-        return userEntity != null && passwordEncoder.matches(userDTO.getPassword(), userEntity.getPassword());
+        // Check if an account is verified or not
+        if (userEntity.getStatus() != UserEntity.Status.APPROVED) {
+            throw new AccountNotApprovedException("Account is not approved or rejected");
+        }
+
+        // Check the password of user accounts see if it matched or not in the database
+        if (!passwordEncoder.matches(loginDTO.getPassword(), userEntity.getPassword())) {
+            throw new AuthenticationFailedException("Invalid credentials");
+        }
     }
 
-    // Save user in the database when admin confirm the account
-    public void save(UserEntity user) {
-        userRepository.save(user);
     }
-}
+
