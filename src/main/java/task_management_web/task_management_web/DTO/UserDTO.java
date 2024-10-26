29da7ec27c43f7d02bf29b8c04b230664af9de89
Dong <nhutdong9a3@gmail.com>
2024-10-26 09:37:34 +0700

    Update UserDTO

--- src/main/java/task_management_web/task_management_web/DTO/UserDTO.java
+++ src/main/java/task_management_web/task_management_web/DTO/UserDTO.java
@@ -13,18 +13,27 @@ public class UserDTO {
 
     @NotBlank(message = "Your full name is required")
     private String full_name;
+
     @NotBlank(message = "Your first name is required")
     private String first_name;
+
     @NotBlank(message = "Your last name is required")
     private String last_name;
+
     @NotBlank(message = "Email is required")
     private String email;
+
     @NotBlank(message = "Password is required")
     private String password;
+
     @NotBlank(message = "Your phone number is required")
     private String phone_number;
+
     private String address;
+
     private String status;
+
     private LocalDate created_at;
+
     private int roleId;
 }
