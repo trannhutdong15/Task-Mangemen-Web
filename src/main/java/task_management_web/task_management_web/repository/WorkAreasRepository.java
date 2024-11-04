600d5785ef1dfa2e874826e8f1f915315765c14d
Dong <nhutdong9a3@gmail.com>
2024-11-04 20:23:56 +0700

    add new UserWorkAreasRepo WorkAreasRepo to repository package

---
+++ src/main/java/task_management_web/task_management_web/repository/WorkAreasRepository.java
@@ -0,0 +1,10 @@
+package task_management_web.task_management_web.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+import task_management_web.task_management_web.entity.WorkAreasEntity;
+
+@Repository
+public interface WorkAreasRepository extends JpaRepository<WorkAreasEntity, String> {
+
+}
