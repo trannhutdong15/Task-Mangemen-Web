12f6f4d8208c3df03510948e1f8792abab5ba8af
Dong <nhutdong9a3@gmail.com>
2024-11-04 20:21:55 +0700

    Update SecurityConfig to addfilter Cookie

--- src/main/java/task_management_web/task_management_web/config/SecurityConfig.java
+++ src/main/java/task_management_web/task_management_web/config/SecurityConfig.java
@@ -5,34 +5,34 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
-import task_management_web.task_management_web.exception.CustomAccessDeniedHandler;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig {
 
     @Bean
-    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+    public SecurityFilterChain securityFilterChain(HttpSecurity http, CookieFilter cookieFilter) throws Exception {
+
         http
                 .csrf(AbstractHttpConfigurer::disable)
+                .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
                 .authorizeHttpRequests(auth -> auth
-                        .requestMatchers("/register", "/login" , "/login_validate" , "register_validate").permitAll()
-                        .requestMatchers("/css/**" , "/js/**" , "/plugin/images/**").permitAll()
-                        .requestMatchers("/admin/**").hasAuthority("Admin") // Hoặc hasRole("ROLE_Admin") nếu có tiền tố
-                        .requestMatchers("/teamleader/**").hasAuthority("TeamLeader") // Hoặc hasRole("ROLE_TeamLeader")
-                        .requestMatchers("/staff/**").hasAuthority("Staff") // Hoặc hasRole("ROLE_Staff")
+                        .requestMatchers("/login", "/register", "/login_validate", "/register_validate").permitAll()
+                        .requestMatchers("/css/**", "/js/**", "/plugin/images/**").permitAll()
+                        .requestMatchers("/home").authenticated()
+                        .requestMatchers("/admin/**").hasAuthority("Admin")
                         .anyRequest().authenticated()
                 )
                 .formLogin(form -> form
-                        .loginPage("/login")
+                        .loginPage("/login") // Đường dẫn đến trang đăng nhập tùy chỉnh
                         .permitAll()
                 )
-                .exceptionHandling(exception -> exception
-                        .accessDeniedHandler(accessDeniedHandler())
-                );
+                .addFilterBefore(cookieFilter , UsernamePasswordAuthenticationFilter.class);
         return http.build();
     }
 
@@ -40,10 +40,4 @@ public class SecurityConfig {
     public PasswordEncoder passwordEncoder() {
         return new BCryptPasswordEncoder();
     }
-
-    @Bean
-    public CustomAccessDeniedHandler accessDeniedHandler() {
-        return new CustomAccessDeniedHandler();
-    }
 }
-
